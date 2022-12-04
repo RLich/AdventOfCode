647f5a5170aa623bbacf44868ba3d9da6eeec956
Rafał Lichocki <rafal.lichocki@gmail.com>
2022-12-04 22:08:54 +0100

    Part 2 of Day 3

--- Day_3/Day_3.py
+++ Day_3/Day_3.py
@@ -38,12 +38,51 @@ for rucksack in rucksacks:
         second_half = rucksack.split(sep=first_half, maxsplit=1)[1]
         assert first_half + second_half == rucksack
         matching_item = return_item_from_both_compartments(compartment_a=first_half,
-                                                 compartment_b=second_half)
+                                                           compartment_b=second_half)
         matching_items_priorities.append(return_item_priority(item=matching_item))
         print("")
     except BaseException:
         print("Stripping failed")
 
 print("Sum of priorities for matching items: %s" % sum(matching_items_priorities))
+print("\nEnd of Part 1\n")
 
 # Part 2:
+
+
+def find_matching_item_in_group(group_rucksacks):
+    print(group_rucksacks)
+    list_rucksack_a, list_rucksack_b, list_rucksack_c = [], [], []
+    list_rucksack_a[:0] = group_rucksacks[0]  # split the string into a list of it's characters
+    list_rucksack_b[:0] = group_rucksacks[1]  # split the string into a list of it's characters
+    list_rucksack_c[:0] = group_rucksacks[2]  # split the string into a list of it's characters
+    for item in list_rucksack_a:
+        if item in list_rucksack_b and item in list_rucksack_c:
+            print("Found a matching item: %s" % item)
+            return item
+
+
+print(rucksacks)
+dict_elements_counter = 0
+groups = {dict_elements_counter: []}
+elf_counter = 0
+for rucksack in rucksacks:
+    elf_counter += 1
+    if elf_counter <= 3:
+        groups[dict_elements_counter].append(rucksack)
+    else:
+        dict_elements_counter += 1
+        groups[dict_elements_counter] = []
+        groups[dict_elements_counter].append(rucksack)
+        elf_counter = 1
+print(groups)
+
+group_items_priority_list = []
+group_counter = 0
+for group in groups:
+    group_matching_item = find_matching_item_in_group(group_rucksacks=groups[group_counter])
+    item_priority = return_item_priority(item=group_matching_item)
+    group_items_priority_list.append(item_priority)
+    group_counter += 1
+
+print("Sum of the priorities for item types is: %s" % sum(group_items_priority_list))
